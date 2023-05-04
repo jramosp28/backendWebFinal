15705bc7b7c4842ca1c72e597da550f69082e6f8
jramosp28 <javierrampob@campus.monlau.com>
2023-05-04 19:48:43 +0200

    creando modelo controlador de usuarios

---
+++ models/userModel.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema(
+    {
+      nombre: String,
+      apellidoP: String,
+      apellidoM: String,
+      username: String,
+      password: String,
+      email: String,
+      fechaN: String,
+      role_id: String,
+    },
+    { versionKey: false }
+  );
+  
+  const User = mongoose.model("User", userSchema);
